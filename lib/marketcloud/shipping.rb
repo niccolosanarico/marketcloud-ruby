00e34456aa100a5b0ead639e12ec02bb6ea67333
Niccolo Sanarico <nico@niccolosanarico.com>
2017-01-18 22:34:59 +0100

    added defaults to shippings, to be further reviewed

--- lib/marketcloud/shipping.rb
+++ lib/marketcloud/shipping.rb
@@ -30,16 +30,16 @@ module Marketcloud
 			@per_item_cost = attributes['per_item_cost']
 			@zones = attributes['zones']
 			@base_cost = attributes['base_cost']
-			@max_weight = attributes['max_weight']
-			@min_weight = attributes['min_weight']
-			@max_depth = attributes['max_depth']
-			@min_depth = attributes['min_depth']
-			@max_height = attributes['max_height']
-			@min_height = attributes['min_height']
-			@max_width = attributes['max_width']
-			@min_width = attributes['min_width']
-			@max_value = attributes['max_value']
-			@min_value = attributes['min_value']
+			@max_weight = attributes['max_weight'] || 10000000000
+			@min_weight = attributes['min_weight'] || 0
+			@max_depth = attributes['max_depth'] || 0
+			@min_depth = attributes['min_depth'] || 0
+			@max_height = attributes['max_height'] || 0
+			@min_height = attributes['min_height'] || 0
+			@max_width = attributes['max_width'] || 0
+			@min_width = attributes['min_width'] || 0
+			@max_value = attributes['max_value'] || 0
+			@min_value = attributes['min_value'] || 0
 		end
 
 		# Find a shipping by ID
